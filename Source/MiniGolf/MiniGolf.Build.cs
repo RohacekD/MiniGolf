f88e8ea3721bdff18edd1bc47cf6d8638678ff67
RohacekD <RohacekD@gmail.com>
2021-08-14 16:09:26 +0200

    * Faster builds?

--- Source/MiniGolf/MiniGolf.Build.cs
+++ Source/MiniGolf/MiniGolf.Build.cs
@@ -10,5 +10,11 @@ public class MiniGolf : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 		PrivateDependencyModuleNames.AddRange(new string[] { "FMODStudio", "Slate", "SlateCore" });
-	}
+
+        // https://forums.unrealengine.com/t/is-c-really-this-slow-in-unreal-4/83905/7#post918034
+        // tanis - start faster compile time for small projects
+        MinFilesUsingPrecompiledHeaderOverride = 1;
+        bFasterWithoutUnity = true;
+        // tanis - end
+    }
 }
